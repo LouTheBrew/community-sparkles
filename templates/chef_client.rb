50120608297d5c7bab40695129a7f0c95730a619
Luis De Siqueira <louthebrew@gmail.com>
2015-12-02 16:32:51 -0800

    updating

--- templates/chef_client.rb
+++ templates/chef_client.rb
@@ -1,6 +1,22 @@
 SparkleFormation.new('chef_client') do
-  description "A Base Chef Client"
+  description "A Chef Client"
   parameters do
+    hostname do
+      type "String"
+      default "cheftestnode"
+    end
+    domain do
+      type "String"
+      default "anaplan-devops.com."
+    end
+    private_hosted_zone_id do
+      type "String"
+      default "Z7VEETB3NASPZ"
+    end
+    public_hosted_zone_id do
+      type "String"
+      default "ZQ3EBPCJHKLF6"
+    end
     owner do
       type "String"
       default "luis"
@@ -13,18 +29,11 @@ SparkleFormation.new('chef_client') do
       type "String"
       default "sparkleformation"
     end
-    hostname do
-      type "String"
-      default "chef_test_node"
-    end
     user_data do
       type "String"
       default <<-EOH
-echo "IDONTBELIEVE IN ANYTHING ANYMORE MAN!!!!!" > /tmp/test1
-echo "IDONTBELIEVE IN ANYTHING ANYMORE MAN!!!!!" > /tmp/test2
-echo "IDONTBELIEVE IN ANYTHING ANYMORE MAN!!!!!" > /tmp/test3
-echo "IDONTBELIEVE IN ANYTHING ANYMORE MAN!!!!!" > /tmp/test4
-echo "IDONTBELIEVE IN ANYTHING ANYMORE MAN!!!!!" > /tmp/test5
+#!/bin/bash
+touch /tmp/test1
 EOH
     end
     key_name do
@@ -34,6 +43,10 @@ EOH
     availability_zone do
       type "String"
       default "us-west-1c"
+    end
+    region do
+      type "String"
+      default "us-west-1"
     end
     image_id do
       type "AWS::EC2::Image::Id"
@@ -45,17 +58,16 @@ EOH
     end
     security_group_ids do
       type "List<AWS::EC2::SecurityGroup::Id>"
-      default "sg-02755467"
+      default "sg-93f4d5f6"
     end
     subnet_id do
       type "String"
-      default "subnet-b04b3dd5"
+      default "subnet-10e99f75"
     end
   end
   dynamic!(:ec2_instance, :chef_client) do
     properties do
       user_data base64!(join!("\n",ref!(:user_data)))
-      #user_data ref!(:user_data)
       key_name ref!(:key_name)
       availability_zone ref!(:availability_zone)
       image_id ref!(:image_id)
@@ -70,18 +82,52 @@ EOH
       ]
     end
   end
+  dynamic!(:recordset, :chef_client_public) do
+    properties do
+      name join!(ref!(:hostname),".",ref!(:domain))
+      TTL "900"
+      type "A"
+      hosted_zone_id ref!(:public_hosted_zone_id)
+      resource_records [
+        attr!(:chef_client_ec2_instance, :public_ip)
+      ]
+    end
+  end
+  dynamic!(:recordset, :chef_client_private) do
+    properties do
+      name join!(ref!(:hostname),".",ref!(:domain))
+      TTL "900"
+      type "A"
+      hosted_zone_id ref!(:private_hosted_zone_id)
+      resource_records [
+        attr!(:chef_client_ec2_instance, :private_ip)
+      ]
+    end
+  end
   outputs do
+    owner do
+      value ref!(:owner)
+    end
+    owner_group do
+      value ref!(:owner_group)
+    end
+    owner_application do
+      value ref!(:owner_application)
+    end
     user_data_script do
-      #value attr!(:chef_client_ec2_instance, :user_data)
       value ref!(:user_data)
     end
     public_ip do
-      #value attr!(:chef_client_ec2_instance, :user_data)
-      value ref!(:user_data)
+      value attr!(:chef_client_ec2_instance, :public_ip)
     end
     private_ip do
-      #value attr!(:chef_client_ec2_instance, :user_data)
-      value ref!(:user_data)
+      value attr!(:chef_client_ec2_instance, :private_ip)
+    end
+    public_record do
+      value ref!(:chef_client_public_recordset)
+    end
+    private_record do
+      value ref!(:chef_client_private_recordset)
     end
   end
 end
--- templates/devops_vpc.rb
+++ templates/devops_vpc.rb
@@ -1,6 +1,10 @@
 SparkleFormation.new('devops_vpc') do
-  description "DevOps network"
+  description "DevOps Testing Network"
   parameters do
+    domain do
+      type "String"
+      default "anaplan-devops.com."
+    end
     owner do
       type "String"
       default "luis"
@@ -13,7 +17,7 @@ SparkleFormation.new('devops_vpc') do
       type "String"
       default "sparkleformation"
     end
-    cidr_block do
+    vpc_cidr_block do
       type "String"
       default "10.1.0.0/16"
     end
@@ -21,14 +25,21 @@ SparkleFormation.new('devops_vpc') do
       type "String"
       default "us-west-1c"
     end
+    region do
+      type "String"
+      default "us-west-1"
+    end
   end
   dynamic!(:ec2_vpc, :sparkleformation_testing) do
     properties do
-      cidr_block ref!(:cidr_block)
+      cidr_block ref!(:vpc_cidr_block)
+      enable_dns_support true
+      enable_dns_hostnames true
+      instance_tenancy "default"
       tags [
         {"Key" => "Owner", "Value" => ref!(:owner)},
         {"Key" => "OwnerGroup", "Value" => ref!(:owner_group)},
-        {"Key" => "OwnerApplication", "Value" => ref!(:owner_application)},
+        {"Key" => "OwnerApplication", "Value" => ref!(:owner_application)}
       ]
     end
   end
@@ -37,7 +48,7 @@ SparkleFormation.new('devops_vpc') do
       tags [
         {"Key" => "Owner", "Value" => ref!(:owner)},
         {"Key" => "OwnerGroup", "Value" => ref!(:owner_group)},
-        {"Key" => "OwnerApplication", "Value" => ref!(:owner_application)},
+        {"Key" => "OwnerApplication", "Value" => ref!(:owner_application)}
       ]
     end
   end
@@ -55,12 +66,81 @@ SparkleFormation.new('devops_vpc') do
       map_public_ip_on_launch true
     end
   end
+  dynamic!(:route_table, :sparkleformation_testing) do
+    properties do
+      vpc_id ref!(:sparkleformation_testing_ec2_vpc)
+      tags [
+        {"Key" => "Owner", "Value" => ref!(:owner)},
+        {"Key" => "OwnerGroup", "Value" => ref!(:owner_group)},
+        {"Key" => "OwnerApplication", "Value" => ref!(:owner_application)}
+      ]
+    end
+  end
+  dynamic!(:subnet_route_table_association, :sparkleformation_testing) do
+    properties do
+      route_table_id ref!(:sparkleformation_testing_route_table)
+      subnet_id ref!(:sparkleformation_testing_subnet)
+    end
+  end
+  dynamic!(:route, :sparkleformation_testing) do
+    depends_on "SparkleformationTestingRouteTable"
+    properties do
+      gateway_id ref!(:sparkleformation_testing_internet_gateway)
+      route_table_id ref!(:sparkleformation_testing_route_table)
+      destination_cidr_block "0.0.0.0/0"
+    end
+  end
+  dynamic!(:hosted_zone, :sparkleformation_testing_public) do
+    properties do
+      name ref!(:domain)
+      hosted_zone_config "Comment" => "A private hosted zone for devops testing"
+      hosted_zone_tags [
+        {"Key" => "Owner", "Value" => ref!(:owner)},
+        {"Key" => "OwnerGroup", "Value" => ref!(:owner_group)},
+        {"Key" => "OwnerApplication", "Value" => ref!(:owner_application)}
+      ]
+      VPCs [
+        {"VPCId" => ref!(:sparkleformation_testing_ec2_vpc), "VPCRegion" => ref!(:region)}
+      ]
+    end
+  end
+  dynamic!(:hosted_zone, :sparkleformation_testing_private) do
+    properties do
+      name ref!(:domain)
+      hosted_zone_config "Comment" => "A public hosted zone for devops testing"
+      hosted_zone_tags [
+        {"Key" => "Owner", "Value" => ref!(:owner)},
+        {"Key" => "OwnerGroup", "Value" => ref!(:owner_group)},
+        {"Key" => "OwnerApplication", "Value" => ref!(:owner_application)}
+      ]
+      # A hosted zone is public implicitly when you do not specify a VPC list
+      # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-route53-hostedzone.html
+      #vpcs [
+      #  {"VPCId" => ref!(:sparkleformation_testing_ec2_vpc), "VPCRegion" => ref!(:region)}
+      #]
+    end
+  end
   outputs do
+    owner do
+      value ref!(:owner)
+    end
+    owner_group do
+      value ref!(:owner_group)
+    end
+    owner_application do
+      value ref!(:owner_application)
+    end
     sparkleformation_subnet_id do
       value ref!(:sparkleformation_testing_subnet)
     end
     sparkleformation_vpc_id do
       value ref!(:sparkleformation_testing_ec2_vpc)
+    end
+    public_hosted_zone_id do
+      value ref!(:sparkleformation_testing_public_hosted_zone)
+    end
+    private_hosted_zone_id do
+      value ref!(:sparkleformation_testing_private_hosted_zone)
     end
   end
 end
